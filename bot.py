94158c2f644adeb676c7bf21468b2915f227fa89
Xushnudd <boshhacker1@gmail.com>
2026-04-19 21:35:00 +0500

    done

--- bot.py
+++ bot.py
@@ -1,7 +1,12 @@
 import os
 import asyncio
 from dotenv import load_dotenv
+
+from aiohttp import web
+
 from aiogram import Bot, Dispatcher
+from aiogram.types import Update
+
 from handlers import start, aiChat, pdfCreate
 
 load_dotenv()
@@ -14,8 +19,19 @@ dp.include_router(start.router)
 dp.include_router(aiChat.router)
 dp.include_router(pdfCreate.router)
 
-async def main():
-    await dp.start_polling(bot)
+# async def main():
+#     await dp.start_polling(bot)
     
-if __name__ == "__main__":
-    asyncio.run(main())
+# if __name__ == "__main__":
+#     asyncio.run(main())
+
+async def handle(request):
+    data = await request.json()
+    update = Update.model_validate(data)
+    await dp.feed_update(bot, update)
+    return web.Response(text="ok")
+
+app = web.Application()
+app.router.add_post("/webhook", handle)
+
+web.run_app(app, port=5000)
